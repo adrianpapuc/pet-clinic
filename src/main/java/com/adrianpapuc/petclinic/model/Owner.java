010904ef1e7d27a881ac2fa2624b8f9a6bfa03f8
adrianpapuc <adrianpapuc1999@gmail.com>
2020-09-15 17:10:45 +0300

    adding POJOs. Closes #2.

---
+++ src/main/java/com/adrianpapuc/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.adrianpapuc.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/adrianpapuc/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.adrianpapuc.petclinic.model;
+
+public class Vet extends Person {
+
+}
