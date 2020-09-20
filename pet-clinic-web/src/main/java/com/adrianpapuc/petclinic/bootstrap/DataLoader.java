5169bb5dcf5a8313984c0150edaeea8c125665ca
adrianpapuc <adrianpapuc1999@gmail.com>
2020-09-20 16:51:27 +0300

    Added Spring Config. Closes #21.

--- pet-clinic-web/src/main/java/com/adrianpapuc/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/adrianpapuc/petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.adrianpapuc.petclinic.model.Owner;
 import com.adrianpapuc.petclinic.model.Vet;
 import com.adrianpapuc.petclinic.services.OwnerService;
 import com.adrianpapuc.petclinic.services.VetService;
-import com.adrianpapuc.petclinic.services.map.OwnerServiceMap;
-import com.adrianpapuc.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +13,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
-
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
