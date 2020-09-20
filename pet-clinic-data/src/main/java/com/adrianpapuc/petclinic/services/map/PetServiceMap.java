fdd01ef2d53215eedca5742fe39b720583f21467
adrianpapuc <adrianpapuc1999@gmail.com>
2020-09-20 16:51:01 +0300

    Added Spring Config. Closes #21.

--- pet-clinic-data/src/main/java/com/adrianpapuc/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/adrianpapuc/petclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package com.adrianpapuc.petclinic.services.map;
 
 import com.adrianpapuc.petclinic.model.Pet;
 import com.adrianpapuc.petclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
 
     @Override
