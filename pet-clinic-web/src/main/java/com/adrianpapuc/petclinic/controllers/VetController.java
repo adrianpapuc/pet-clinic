98993cda36feaec3b9212dac134755d71b5ba2cb
adrianpapuc <adrianpapuc1999@gmail.com>
2020-09-20 17:12:42 +0300

    Added Vets list and updated controller. Closes #14.

--- pet-clinic-web/src/main/java/com/adrianpapuc/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/adrianpapuc/petclinic/controllers/VetController.java
@@ -1,12 +1,22 @@
 package com.adrianpapuc.petclinic.controllers;
 
+import com.adrianpapuc.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String vets(){
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"","/index","/index.html"})
+    public String vets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
