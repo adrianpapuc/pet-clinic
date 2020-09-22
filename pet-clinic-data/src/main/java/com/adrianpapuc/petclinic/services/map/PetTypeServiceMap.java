c7612f88932b9996cf9a238a632aa083974bc08c
adrianpapuc <adrianpapuc1999@gmail.com>
2020-09-22 21:05:33 +0300

    updated Pet Type and Specialty services

--- pet-clinic-data/src/main/java/com/adrianpapuc/petclinic/services/map/PetTypeServiceMap.java
+++ pet-clinic-data/src/main/java/com/adrianpapuc/petclinic/services/map/PetTypeServiceMap.java
@@ -2,9 +2,11 @@ package com.adrianpapuc.petclinic.services.map;
 
 import com.adrianpapuc.petclinic.model.PetType;
 import com.adrianpapuc.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetTypeServiceMap extends AbstractMapService<PetType,Long> implements PetTypeService {
     @Override
     public Set<PetType> findAll() {
--- pet-clinic-data/src/main/java/com/adrianpapuc/petclinic/services/map/SpecialtyServiceMap.java
+++ pet-clinic-data/src/main/java/com/adrianpapuc/petclinic/services/map/SpecialtyServiceMap.java
@@ -2,9 +2,11 @@ package com.adrianpapuc.petclinic.services.map;
 
 import com.adrianpapuc.petclinic.model.Specialty;
 import com.adrianpapuc.petclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class SpecialtyServiceMap extends AbstractMapService<Specialty,Long> implements SpecialtyService {
     @Override
     public Set<Specialty> findAll() {
