1984be76c55bbdbd6d7f72812a163146e0e65b59
adrianpapuc <adrianpapuc1999@gmail.com>
2020-10-22 21:07:55 +0300

    refactored for project lombok, closes #50

--- pet-clinic-data/src/main/java/com/adrianpapuc/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/adrianpapuc/petclinic/model/Owner.java
@@ -1,6 +1,9 @@
 package com.adrianpapuc.petclinic.model;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -9,11 +12,18 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
+
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String adress, String city, String tehephone, Set<Pet> pets) {
+        super(id,firstName, lastName);
+        this.adress = adress;
+        this.city = city;
+        this.tehephone = tehephone;
+        this.pets = pets;
+    }
 
     @Column(name = "adress")
     private String adress;
